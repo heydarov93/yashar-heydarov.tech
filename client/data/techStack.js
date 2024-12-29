72e62dfab0639e78e6e40b6d23ba1492d47246dd
heydarov93 <heyderov.yashar@mail.ru>
2024-12-29 20:26:34 +0400

    some static datas created

---
+++ client/data/techStack.js
@@ -0,0 +1,149 @@
+const techStack = [
+  {
+    id: 1,
+    name: 'JavaScript',
+    logo: 'path/to/javascript-logo.svg',
+  },
+  {
+    id: 2,
+    name: 'React',
+    logo: 'path/to/react-logo.svg',
+  },
+  {
+    id: 3,
+    name: 'Node.js',
+    logo: 'path/to/nodejs-logo.svg',
+  },
+  {
+    id: 4,
+    name: 'MongoDB',
+    logo: 'path/to/mongodb-logo.svg',
+  },
+  {
+    id: 5,
+    name: 'HTML',
+    logo: 'path/to/html-logo.svg',
+  },
+  {
+    id: 6,
+    name: 'CSS',
+    logo: 'path/to/css-logo.svg',
+  },
+  {
+    id: 7,
+    name: 'TypeScript',
+    logo: 'path/to/typescript-logo.svg',
+  },
+  {
+    id: 8,
+    name: 'Sass',
+    logo: 'path/to/sass-logo.svg',
+  },
+  {
+    id: 9,
+    name: 'Tailwind',
+    logo: 'path/to/tailwind-logo.svg',
+  },
+  {
+    id: 10,
+    name: 'Redux',
+    logo: 'path/to/redux-logo.svg',
+  },
+  {
+    id: 11,
+    name: 'Git',
+    logo: 'path/to/git-logo.svg',
+  },
+  {
+    id: 12,
+    name: 'GitHub',
+    logo: 'path/to/github-logo.svg',
+  },
+  {
+    id: 13,
+    name: 'NPM',
+    logo: 'path/to/npm-logo.svg',
+  },
+  {
+    id: 14,
+    name: 'C',
+    logo: 'path/to/c-logo.svg',
+  },
+  {
+    id: 15,
+    name: 'C++',
+    logo: 'path/to/cpp-logo.svg',
+  },
+  {
+    id: 16,
+    name: 'Python',
+    logo: 'path/to/python-logo.svg',
+  },
+  {
+    id: 17,
+    name: 'Java',
+    logo: 'path/to/java-logo.svg',
+  },
+  {
+    id: 18,
+    name: 'Django',
+    logo: 'path/to/django-logo.svg',
+  },
+  {
+    id: 19,
+    name: 'Flask',
+    logo: 'path/to/flask-logo.svg',
+  },
+  {
+    id: 20,
+    name: 'Express',
+    logo: 'path/to/express-logo.svg',
+  },
+  {
+    id: 21,
+    name: 'Bootstrap',
+    logo: 'path/to/bootstrap-logo.svg',
+  },
+  {
+    id: 22,
+    name: 'Material-UI',
+    logo: 'path/to/material-ui-logo.svg',
+  },
+  {
+    id: 23,
+    name: 'Ant Design',
+    logo: 'path/to/ant-design-logo.svg',
+  },
+  {
+    id: 24,
+    name: 'Jest',
+    logo: 'path/to/jest-logo.svg',
+  },
+  {
+    id: 25,
+    name: 'Docker',
+    logo: 'path/to/docker-logo.svg',
+  },
+  {
+    id: 26,
+    name: 'Linux',
+    logo: 'path/to/linux-logo.svg',
+  },
+  {
+    id: 27,
+    name: 'Ubuntu',
+    logo: 'path/to/ubuntu-logo.svg',
+  },
+  {
+    id: 28,
+    name: 'MySQL',
+    logo: 'path/to/mysql-logo.svg',
+  },
+  {
+    id: 29,
+    name: 'PostgreSQL',
+    logo: 'path/to/postgresql-logo.svg',
+  },
+];
+
+export default techStack;
