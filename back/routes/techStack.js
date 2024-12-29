c2ed4e5c1ccc6d63f286d8f497036877a1f530be
heydarov93 <heyderov.yashar@mail.ru>
2024-12-29 20:43:27 +0400

    routers for tech stack created

---
+++ back/routes/techStack.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import db from '../db/conn.js';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const mainTechs = db.collection('main_tech_stack');
+  const mainCursor = mainTechs.find({});
+  const mainResults = await mainCursor.toArray();
+
+  const secondaryTechs = db.collection('secondary_tech_stack');
+  const secondaryCursor = secondaryTechs.find({});
+  const secondaryResults = await secondaryCursor.toArray();
+
+  res.send([...mainResults, ...secondaryResults]).status(200);
+});
+
+router.get('/main', async (req, res) => {
+  const mainTechStack = db.collection('main_tech_stack');
+  const cursor = mainTechStack.find({});
+  const results = await cursor.toArray();
+
+  res.send(results).status(200);
+});
+
+router.get('/secondary', async (req, res) => {
+  const secondaryTechStack = db.collection('secondary_tech_stack');
+  const cursor = secondaryTechStack.find({});
+  const results = await cursor.toArray();
+
+  res.send(results).status(200);
+});
