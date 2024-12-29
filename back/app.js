808f99f6bedaa4049fca17c500f53779a782bfce
heydarov93 <heyderov.yashar@mail.ru>
2024-12-29 21:49:47 +0400

    handling base endpoints for hero, about, tech-staxk, experiences

--- back/app.js
+++ back/app.js
@@ -1,6 +1,10 @@
 import './loadEnvironments.js';
 import express from 'express';
 import menus from './routes/menus.js';
+import hero from './routes/hero.js';
+import about from './routes/about.js';
+import techStack from './routes/techStack.js';
+import experiences from './routes/experiences.js';
 
 const app = express();
 
@@ -10,6 +14,14 @@ const port = 3000;
 app.use(express.json());
 
 app.use('/menus', menus);
+
+app.use('/hero', hero);
+
+app.use('/about', about);
+
+app.use('/tech-stack', techStack);
+
+app.use('/experiences', experiences);
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
