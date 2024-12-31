f3c87c7f0a2526dbdd84a321f0ca2f63128cb2e8
heydarov93 <heyderov.yashar@mail.ru>
2024-12-31 18:21:00 +0400

    base class for api actions

---
+++ client/js/api/ApiService.js
@@ -0,0 +1,31 @@
+'use strict';
+
+class ApiService {
+  #baseUrl;
+
+  constructor(baseUrl) {
+    this.#baseUrl = baseUrl;
+  }
+
+  async request(endpoint, method = 'GET', data = null, headers = {}) {
+    const options = {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+        ...headers,
+      },
+    };
+
+    if (data) options.body = JSON.stringify(data);
+
+    const response = await fetch(`${this.#baseUrl}${endpoint}`, options);
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    return response;
+  }
+}
+
+export default ApiService;
