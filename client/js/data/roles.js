ab92a03da2619c3ee651de341b05b224080e864b
heydarov93 <heyderov.yashar@mail.ru>
2025-01-05 21:12:04 +0400

    roles file created

---
+++ client/js/data/roles.js
@@ -0,0 +1,13 @@
+const roles = [
+  { id: 1, role: 'Frontend Development' },
+  { id: 2, role: 'Backend Development' },
+  { id: 3, role: 'Full Stack Development' },
+  { id: 4, role: 'DevOps' },
+  { id: 5, role: 'QA Testing' },
+  { id: 6, role: 'Project Management' },
+  { id: 7, role: 'UI/UX Design' },
+  { id: 8, role: 'Tech Lead' },
+  { id: 9, role: 'Team Lead' },
+];
+
+export default roles;
