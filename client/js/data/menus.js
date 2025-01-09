9fc20e38f31e45c06f22595cdffe5e97069e7847
heydarov93 <heyderov.yashar@mail.ru>
2025-01-09 20:01:55 +0400

    file contains menus for navbars of header and footer

---
+++ client/js/data/menus.js
@@ -0,0 +1,63 @@
+export const homeMenus = {
+  auth: [
+    {
+      name: 'Login',
+      link: '/login',
+    },
+    {
+      name: 'Register',
+      link: '/register',
+    },
+    {
+      name: 'Logout',
+      link: '/logout',
+    },
+  ],
+  navItems: [
+    {
+      name: 'Home',
+      link: '/',
+    },
+    {
+      name: 'About',
+      link: '#aboutSection',
+    },
+    {
+      name: 'Experience',
+      link: '#experienceSection',
+    },
+    {
+      name: 'Projects',
+      link: '#portfolioSection',
+    },
+    {
+      name: 'Tech Stack',
+      link: '#techStackSection',
+    },
+    {
+      name: 'Blog',
+      link: '/blogs',
+    },
+    {
+      name: 'Education',
+      link: '#eduSection',
+    },
+    {
+      name: 'Contact',
+      link: '#contactSection',
+    },
+  ],
+};
+
+export const otherPageMenus = {
+  auth: homeMenus.auth,
+  navItems: homeMenus.navItems.map((item) => {
+    if (item.name !== 'Home')
+      return {
+        name: item.name,
+        link: `/${item.link}`,
+      };
+
+    return item;
+  }), // copy the array
+};
