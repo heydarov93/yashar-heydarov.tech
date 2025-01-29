2a65199adca0d63024a9836d4c7504d4c76cc0f8
heydarov93 <heyderov.yashar@mail.ru>
2025-01-29 19:06:14 +0400

    getAll implemented with try/catch, method created for sending email

--- client/js/api/ContactApi.js
+++ client/js/api/ContactApi.js
@@ -6,8 +6,27 @@ class ContactApi extends ApiService {
     super(API_Config.BASE_URL);
   }
 
-  getAll() {
-    return this.request('/contact');
+  async getAll() {
+    try {
+      const response = await this.request('/contact');
+      return response;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async sendEmail(from, name, message) {
+    try {
+      const response = await this.request('/contact/send-email', 'POST', {
+        from,
+        name,
+        message,
+      });
+
+      return response;
+    } catch (err) {
+      throw err;
+    }
   }
 }
 
