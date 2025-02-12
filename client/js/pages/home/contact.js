f8029f025ff9b76e84a4d3d13f02703d08518f90
heydarov93 <heyderov.yashar@mail.ru>
2025-02-12 21:04:39 +0400

    custom error class modified in order to store error data, form validation, error handling, displaying error messages modified and optimized

--- client/js/pages/home/contact.js
+++ client/js/pages/home/contact.js
@@ -1,11 +1,13 @@
 import ContactApi from '../../api/ContactApi.js';
+import Notification from '../../components/Notification.js';
+import { sanitizeInput } from '../../helpers/sanitizeInput.js';
 import AbstractHome from './AbstractHome.js';
 
 class ContactError extends Error {
-  constructor(message, stack) {
+  constructor(message, errorData = null) {
     super(message);
     this.name = 'ContactError';
-    this.stack = stack;
+    this.errorData = errorData;
   }
 }
 
@@ -15,6 +17,8 @@ class Contact extends AbstractHome {
   contactSectionEl;
   socialsContainer;
   formEl;
+  submitBtn;
+  #formNotification;
   #formInputInfos = {
     name: {
       value: '',
@@ -37,6 +41,7 @@ class Contact extends AbstractHome {
     this.contactSectionEl = document.querySelector('#contactSection');
     this.socialsContainer = this.contactSectionEl.querySelector('.socials');
     this.formEl = this.contactSectionEl.querySelector('#contactForm');
+    this.submitBtn = this.contactSectionEl.querySelector('.form-btn');
 
     this.formEl.addEventListener('submit', this.onSubmit.bind(this));
 
@@ -50,6 +55,11 @@ class Contact extends AbstractHome {
 
     // Initialize API
     this.#contactApi = new ContactApi();
+
+    // create notification popup (instance) in order to notify user
+    // about status of the sending message
+    // but don't show it yet
+    this.#formNotification = new Notification();
   }
 
   // initializes contact section and updates html
@@ -59,7 +69,7 @@ class Contact extends AbstractHome {
         this.#data = contactData;
         this.updateDom();
       } catch (err) {
-        throw new ContactError(err.message, err.stack);
+        throw new ContactError(err.message);
       }
     }
   }
@@ -102,10 +112,22 @@ class Contact extends AbstractHome {
     try {
       const response = await this.#contactApi.sendEmail(from, name, message);
 
-      if (!response.ok)
-        throw new ContactError(`Email not sent: ${response.status}`);
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new ContactError('Validation failed', {
+          status: response.status,
+          errors: errorData.errors,
+        });
+      }
+
+      return response;
     } catch (err) {
-      throw new ContactError(err.message, err.stack);
+      // if the error is a ContactError, rethrow it
+      if (err instanceof ContactError) {
+        throw err;
+      }
+
+      throw new ContactError(err.message);
     }
   }
 
@@ -113,25 +135,61 @@ class Contact extends AbstractHome {
   async onSubmit(event) {
     event.preventDefault();
 
+    // clear all previous error messages
+    this.#clearErrorMessages();
+
+    // make button disabled on submit
+    // prevents double submission
+    this.submitBtn.disabled = true;
+
+    // get all data from inputs and store them inside instance
     const form = event.target;
     const formData = new FormData(form);
-    this.#formInputInfos.name.value = formData.get('name');
-    this.#formInputInfos.from.value = formData.get('email');
-    this.#formInputInfos.message.value = formData.get('message');
+    this.#formInputInfos.name.value = sanitizeInput(
+      formData.get('name')?.trim()
+    );
+    this.#formInputInfos.from.value = sanitizeInput(
+      formData.get('email')?.trim()
+    );
+    this.#formInputInfos.message.value = sanitizeInput(
+      formData.get('message')?.trim()
+    );
 
+    const { name, from, message } = this.#formInputInfos;
     try {
       // validate form fields
-      if (!this.validateForm()) return;
+      if (!this.validateForm()) throw new Error();
 
       // send email to backend
-      await this.sendEmail(from, name, message);
+      await this.sendEmail(from.value, name.value, message.value);
+
+      // show pop up with status information
+      this.#formNotification.show('Email sent successfully!', 'success');
 
       // clear form fields
       form.reset();
-      console.log('Email sent successfully');
     } catch (err) {
-      console.warn('Something went wrong. Please try again later');
-      throw new ContactError(err.message, err.stack);
+      // First, hide the notification
+      // This prevents UI bugs in case of multiple sequential clicks
+      this.#formNotification.hide();
+
+      // show pop up with status information
+      this.#formNotification.show(
+        'Something went wrong. Please try again.',
+        'error'
+      );
+
+      // if validation fails in the server show errors
+      // with the relative messages from backend
+      if (err instanceof ContactError && err?.errorData?.errors) {
+        err.errorData.errors.forEach((errInfo) => {
+          this.#formInputInfos[errInfo.inputName].errorTextEl.innerHTML +=
+            errInfo.message + '<br>';
+        });
+      }
+    } finally {
+      // at the end make button useful again
+      this.submitBtn.disabled = false;
     }
   }
 
@@ -139,22 +197,25 @@ class Contact extends AbstractHome {
   validateForm() {
     const { name, from: email, message } = this.#formInputInfos;
 
-    // clear previous error messages
-    name.errorTextEl.textContent =
-      email.errorTextEl.textContent =
-      message.errorTextEl.textContent =
-        '';
+    // check if name is too short or too long
+    if (name.value.length < 3 || name.value.length > 30)
+      name.errorTextEl.innerHTML +=
+        'Name should be between 3 and 30 characters!</br>';
 
-    // check if any field is empty and show error message if so
-    name.errorTextEl.textContent = !name.value?.trim()
-      ? 'Please enter your name'
-      : '';
-    email.errorTextEl.textContent = !email.value?.trim()
-      ? 'Please enter your email'
-      : '';
-    message.errorTextEl.textContent = !message.value?.trim()
-      ? 'Please enter your message'
-      : '';
+    // allow letters and whitespace in name, but not whitespace-only
+    if (!/^[A-Za-z\s]+$/.test(name.value))
+      name.errorTextEl.innerHTML +=
+        'Name should contain only letters and spaces!</br>';
+
+    // check if email is valid and show error message if not
+    const isValidEmail = this.#validateEmail(email.value);
+
+    if (!isValidEmail) email.errorTextEl.textContent = 'Invalid email address.';
+
+    // check if message is too short or too long
+    if (message.value.length < 10 || message.value.length > 2500)
+      message.errorTextEl.textContent =
+        'Message should be between 10 and 2500 characters!';
 
     // check if any error message is present in any field and return false
     if (
@@ -163,28 +224,6 @@ class Contact extends AbstractHome {
       message.errorTextEl.textContent
     )
       return false;
-
-    // check if email is valid and show error message if not
-    const isValidEmail = this.#validateEmail(email.value);
-
-    if (!isValidEmail) {
-      email.errorTextEl.textContent = 'Please enter a valid email';
-      return false;
-    }
-
-    // check if name is too short or too long
-    if (name.value.length < 3 || name.value.length > 20) {
-      name.errorTextEl.textContent =
-        'Name should be between 3 and 20 characters';
-      return false;
-    }
-
-    // check if message is too short or too long
-    if (message.value.length < 10 || message.value.length > 1000) {
-      message.errorTextEl.textContent =
-        'Message should be between 10 and 1000 characters';
-      return false;
-    }
 
     return true;
   }
@@ -196,6 +235,13 @@ class Contact extends AbstractHome {
       .match(
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       );
+  }
+
+  // clear error messages of the inputs
+  #clearErrorMessages() {
+    Object.keys(this.#formInputInfos).forEach(
+      (key) => (this.#formInputInfos[key].errorTextEl.textContent = '')
+    );
   }
 }
 
