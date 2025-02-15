421b3edad4f5eadf7a8e536e6f1954ecf710cc8f
heydarov93 <heyderov.yashar@mail.ru>
2025-02-15 13:24:09 +0400

    Add comments, move code into try/catch block, and modify error message

--- back/helpers/rate-limiters.js
+++ back/helpers/rate-limiters.js
@@ -1,29 +1,47 @@
 import getIp from './getIp.js';
+import HttpError from './HttpError.js';
 
 const trackers = {}; // {ip-address : {count: number, expiresAt: number}}
 
 function rateLimitByIp(req, limit, interval) {
-  const ip = getIp(req);
+  try {
+    // Get the client's IP address
+    const ip = getIp(req);
 
-  if (!ip) throw new Error('Ip address not found');
+    if (!ip) throw new HttpError('Ip address not found', 400);
 
-  const tracker = trackers[ip] || { count: 0, expiresAt: 0 };
+    // Get the tracker for the IP address or create a new one if it doesn't exist
+    const tracker = trackers[ip] || { count: 0, expiresAt: 0 };
 
-  if (!trackers[ip]) trackers[ip] = tracker;
+    // If the IP address doesn't exist in the trackers object, add the new tracker
+    if (!trackers[ip]) trackers[ip] = tracker;
 
-  if (tracker.expiresAt < Date.now()) {
-    tracker.count = 0;
-    tracker.expiresAt = Date.now() + interval;
+    // Reset the tracker if the interval has expired
+    if (tracker.expiresAt < Date.now()) {
+      tracker.count = 0;
+      tracker.expiresAt = Date.now() + interval;
+    }
+
+    // Increment the request count for the IP address
+    tracker.count++;
+
+    // Throw a 429 Too Many Requests error if the rate limit is exceeded
+    if (tracker.count > limit) {
+      let minutesLeft = Math.ceil((tracker.expiresAt - Date.now()) / 1000 / 60);
+
+      throw new HttpError(
+        `Rate limit exceeded. Please try again in ${minutesLeft} minutes.`,
+        429
+      );
+    }
+  } catch (err) {
+    // If the error is not an instance of HttpError, wrap it in a 500 Internal Server Error
+    if (!(err instanceof HttpError))
+      throw new HttpError('Internal Server Error', 500);
+
+    // Re-throw the error
+    throw err;
   }
-
-  tracker.count++;
-
-  if (tracker.count > limit)
-    throw new Error(
-      `Rate limit exceeded. Please try again in ${
-        interval / 1000 / 60
-      } minutes.`
-    );
 }
 
 export { rateLimitByIp };
